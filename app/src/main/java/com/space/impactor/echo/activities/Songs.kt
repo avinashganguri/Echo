f6631a4e1a84fb57de738c11b6b2aa7105cc7890
Avi <avinashganguri@gmail.com>
2018-09-17 14:12:50 +0530

    Initial commit

---
+++ app/src/main/java/com/space/impactor/echo/activities/Songs.kt
@@ -0,0 +1,10 @@
+package com.space.impactor.echo.activities
+
+/**
+ * Created by Avi on 13/08/18.
+ */
+
+class Songs(var SongID: Long, var songTitle : String, var artist: String, var songData: String, var dateAdded: Long)
+{
+
+}
